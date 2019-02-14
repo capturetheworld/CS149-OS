ce9acae3c1eed2a7ad868e5d352449f4e764f4ea
Ian SooHoo <ian@iansoohoo.me>
2019-02-14 04:02:23 -0800

    add a bunch

--- FastFactor.c
+++ FastFactor.c
@@ -3,5 +3,18 @@
 int main(int argc, char *argv[]){
 
 	long long input;
-	
+
+
+	 for (int i=0; i < argc; i++)
+		{
+			if(argv[i] ==NaN || argv[i] <= 0){
+				printf("Error, only enter numbers and positive numbers greater than 0. %s\n");
+				exit();
+
+			}
+
+
+		}
+
+
 }
