5fa0fe7d70f1ddb5a3952574982c4d4ed9064479
Ian SooHoo <ian@iansoohoo.me>
2019-02-14 17:02:38 -0800

    update digit

--- FastFactor.c
+++ FastFactor.c
@@ -16,12 +16,12 @@ printf("argc %d\n", argc);
 			if(input <= 0){
 				printf("the int %s\n", argv[i]);
 				printf("%s\n", "Error, only enter numbers and positive numbers greater than 0.");
-				exit();
+				exit( );
 		
 			}
-			if !(isDigit(input)){
+			if (!(isDigit(input)){
 				printf("%s\n", "Error, only enter numerical numbers, no symbols or other characters.");
-				exit();
+				exit( );
 			}
 
 			int finalfactors[];
