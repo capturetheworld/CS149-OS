d5c0a6383c95136820dd0c3fb247c037b25f0d79
Ian SooHoo <ian@iansoohoo.me>
2019-02-14 22:33:08 -0800

    Update FastFactor.c

--- FastFactor.c
+++ FastFactor.c
@@ -1,4 +1,7 @@
-#include < stdio.h > #include < stdlib.h > /* strtoll */ #include < ctype.h > #include < unistd.h >
+#include < stdio.h > 
+#include < stdlib.h > /* strtoll */ 
+#include < ctype.h > 
+#include < unistd.h >
 
   int main(int argc, char * argv[]) {
 
