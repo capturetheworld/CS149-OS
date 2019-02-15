c5c2c838641afa4361fe4efb2d3f28cb338dec53
Ian SooHoo <ian@iansoohoo.me>
2019-02-14 21:34:59 -0800

    Update FastFactor.c

--- FastFactor.c
+++ FastFactor.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]){
 		
 			}
 			if (isdigit(input) == 0){
-				printf("%d\n", isdigit(input));
+				printf("%d\n", input);
 
 				printf("%s\n", "Error, only enter numerical numbers, no symbols or other characters.");
 				exit(0);
