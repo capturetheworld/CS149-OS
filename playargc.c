3cf743b36742ce55a9ba02d5365a68f3a80b1645
Ian SooHoo <ian@iansoohoo.me>
2019-02-23 00:03:03 -0800

    what

---
+++ playargc.c
@@ -0,0 +1,43 @@
+#include "util.h"
+
+
+void main(int argc, char * argv[]){
+
+    char *storage = NULL;
+    size_t n =0;
+
+    printf("argc is: %d\n", argc);
+
+
+
+
+
+
+
+    if(argc == 1){
+
+
+        while(getline(&storage, &n, ))
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+    
+
+
+
+
+
+
+}
+
